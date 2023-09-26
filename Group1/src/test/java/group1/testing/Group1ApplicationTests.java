e751e0dc07df8b9ef95d121dedf7ae6d4c0f835a
shhzlhs <hoangonshhz@gmail.com>
2023-09-26 12:41:17 +0700

    26/9/2023 Son

---
+++ Group1/src/test/java/group1/testing/Group1ApplicationTests.java
@@ -0,0 +1,13 @@
+package group1.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
